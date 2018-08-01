772ee50fd646979604f634450e1e9b47d7cff679
Antonio Vasilev <antoniovassilev@gmail.com>
2018-08-01 10:54:52 +0100

    :100: Bringing back old forms

--- components/Popup.js
+++ components/Popup.js
@@ -14,26 +14,12 @@ class PopUp extends Component{
                 <p>
                   Silvana is currently re-building and improving her website. If you would like to stay up-to-date with us and get notified when she is ready to ship then please give us your email.
                 </p>
-                <form name="contact" method="POST" netlify>
-                  <p>
-                    <label>Your Name: <input type="text" name="name" /></label>   
-                  </p>
-                  <p>
-                    <label>Your Email: <input type="email" name="email" /></label>
-                  </p>
-                  <p>
-                    <label>Your Role: <select name="role[]" multiple>
-                      <option value="leader">Leader</option>
-                      <option value="follower">Follower</option>
-                    </select></label>
-                  </p>
-                  <p>
-                    <label>Message: <textarea name="message"></textarea></label>
-                  </p>
-                  <p>
-                    <button type="submit">Send</button>
-                  </p>
-                </form>
+              <form className="ss-form" action="https://silvanasahagart.us17.list-manage.com/subscribe/post" method="POST" name="Sign Up User" _lpchecked={1}>
+                  <input type="hidden" name="u" defaultValue="854fb6542dc8e5dc4a4a1a30f" />
+                  <input type="hidden" name="id" defaultValue="bc3216ec1f" />
+                  <input className="form-control ss-form-email" type="email" autoCapitalize="off" autoCorrect="off" name="MERGE0" id="MERGE0" size={25} placeholder="Please Enter Your Email Address"/>
+                  <button className="ss-standard-cta-link ss-standard-cta-link__primary-cta ss-standard-cta-link__primary-cta-promotion" type="submit">Sign Up</button>
+              </form>
               </div>
             </div>
           </div>
--- components/SignUp.js
+++ components/SignUp.js
@@ -4,20 +4,21 @@ import React, { Component } from 'react';
 class SignUp extends Component{
     render() {
         return(
-            <section class="ss-component ss-section-sign-up">
-            <h2 class="ss-component-update-form-heading">Sign Up For Updates</h2>
-            <section class="ss-component-update-form clearfix">
-                <aside class="ss-section-sign-up-disclaimer">
-                    <p class="ss-light-copy ss-body-style">Silvana is currently re-building and improving her website. If you would like to stay up-to-date with us and get notified of any up-and-coming exhibitions then, please - sign up.</p>
-                </aside>
-                <form name="sssignup" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-                  <input type="hidden" name="bot-field" />
-                    <input required type="email" class="form-control ss-form-email md-col-9" autoCapitalize="off" autoCorrect="off" name="MERGE0" id="MERGE0" size="25" placeholder='Email address'/>
-                    <button class="ss-standard-cta-link ss-standard-cta-link__primary-cta ss-standard-cta-link__primary-cta-promotion col-10 md-col-3" type="submit">Sign Up</button>
-                </form>
-                <small id="emailHelp" class="form-text text-muted ss-form-disclaimer">We'll never share your email with anyone else. That's a promise.</small>
+            <section className="ss-component ss-section-sign-up">
+            <h2 className="ss-component-update-form-heading">Sign Up For Updates</h2>
+            <section className="ss-component-update-form clearfix">
+              <aside className="ss-section-sign-up-disclaimer">
+                <p className="ss-light-copy ss-body-style">Silvana is currently re-building and improving her website. If you would like to stay up-to-date with us and get notified of any up-and-coming exhibitions then, please - sign up.</p>
+              </aside>
+              <form className="ss-form" action="https://silvanasahagart.us17.list-manage.com/subscribe/post" method="POST" name="Sign Up User" _lpchecked={1}>
+                <input type="hidden" name="u" defaultValue="854fb6542dc8e5dc4a4a1a30f" />
+                <input type="hidden" name="id" defaultValue="bc3216ec1f" />
+                <input required type="email" className="form-control ss-form-email md-col-9" autoCapitalize="off" autoCorrect="off" name="MERGE0" id="MERGE0" size={25} placeholder="Email address" />
+                <button className="ss-standard-cta-link ss-standard-cta-link__primary-cta ss-standard-cta-link__primary-cta-promotion col-10 md-col-3" type="submit">Sign Up</button>
+              </form>
+              <small id="emailHelp" className="form-text text-muted ss-form-disclaimer">We'll never share your email with anyone else. That's a promise.</small>
             </section>
-        </section>
+          </section>
         )
     }
 }
