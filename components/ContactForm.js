6664b09e5d7e108dd64b8a18bf441c4c92fe1676
Antonio Vasilev <antoniovassilev@gmail.com>
2018-08-01 11:06:12 +0100

    :100: Adding in basic form

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react';
 class ContactForm extends Component{
     render(){
         return (
-        <form>
-            
+        <section>
+        <p>If you are interested in forthcoming exhibitions, purchasing work or visiting the studio please contact me:</p>
+        <form>        
+            <input required type="text" className="form-control ss-form-firstName md-col-5" autoCapitalize="off" autoCorrect="off" name="MERGE0" id="MERGE0" size={25} placeholder="First Name" />
+            <input required type="text" className="form-control ss-form-lastName md-col-5" autoCapitalize="off" autoCorrect="off" name="MERGE0" id="MERGE0" size={25} placeholder="Last Name" />
+            <input required type="email" className="form-control ss-form-email md-col-12" autoCapitalize="off" autoCorrect="off" name="MERGE0" id="MERGE0" size={25} placeholder="Email" />
+            <input required type="text" className="form-control ss-form-subject md-col-12" autoCapitalize="off" autoCorrect="off" name="MERGE0" id="MERGE0" size={25} placeholder="Subject" />
+            <input required type="textarea" className="form-control ss-form-message md-col-12" autoCapitalize="off" autoCorrect="off" name="MERGE0" id="MERGE0" size={25} placeholder="Message" />
+            <button className="ss-standard-cta-link ss-standard-cta-link__primary-cta ss-standard-cta-link__primary-cta-promotion" type="submit">Submit</button>
         </form>
+        </section>
         );
     }
 }
